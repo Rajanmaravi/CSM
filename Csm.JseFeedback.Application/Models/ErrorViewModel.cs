7239a5633ed746d0bf4ad02b01a74723fd4773f7
Rajanmaravi <69163079+Rajanmaravi@users.noreply.github.com>
2024-04-18 17:18:03 +0530

    Delete Csm.JseFeedback.Application directory

--- Csm.JseFeedback.Application/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Csm.JseFeedback.Application.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
